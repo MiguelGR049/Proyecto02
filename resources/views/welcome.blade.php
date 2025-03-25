3f59441b34fbd50364584792b72517c2cdc2ec10
MiguelGR049 <144866412+MiguelGR049@users.noreply.github.com>
2025-03-25 08:10:56 -0600

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,10 @@
             <div class="container mt-8">
                 <div class="row mb-3 justify-content-center">
                     <div class="card justify-content-center" style="width: 25rem; background-color: rgba(255, 255, 255, 0.5);">
-                        <img class="mx-auto d-block" src="./public/img/Itadori.png" height="95" width="125px">
+                        <img class="mx-auto d-block" src="{{ asset('img/Itadori.png') }}" height="95" width="125px">
                         <div class="card-body row justify-content-center">
 
-                            <h1 class="fw-bold text-center">Configuracion</h1>
+                            <h1 class="fw-bold text-center">Bienvenidos</h1>
 
                         </div>
                     </div>
