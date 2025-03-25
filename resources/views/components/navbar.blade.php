e7c415a5066280cb1130c0e3c9ac76b61d821157
MiguelGR049 <144866412+MiguelGR049@users.noreply.github.com>
2025-03-24 19:01:14 -0600

    test

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,38 +1,74 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-dark bg-dark fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Dragon Ball</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#" id="navbar_brand">Lista Animes</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar"
+            aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><i class="fa-regular fa-user"></i>Animes</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                    aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a id="home" class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-house"></i> Home</a>
+                    </li>
+                    <li id="item_genero" class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Genero
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" id="genero">
+                            <li><a class="dropdown-item" href="#" data-genero="action">Acción</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="adventure">Aventura</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="comedy">Comedia</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="drama">Drama</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="fantasy">Fantasía</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="romance">Romance</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="sci-fi">Ciencia Ficción</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="horror">Horror</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="mystery">Misterio</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="slice-of-life">Slice of Life</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="shonen">Shonen</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="shoujo">Shoujo</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="seinen">Seinen</a></li>
+                            <li><a class="dropdown-item" href="#" data-genero="josei">Josei</a></li>
+                        </ul>
+                    </li>
+                    <li id="item_visto" class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Visto
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" id="visto">
+                            <li><a class="dropdown-item" href="#" data-visto="si">Si</a></li>
+                            <li><a class="dropdown-item" href="#" data-visto="no">No</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex mt-2 mb-2" role="search">
+                    <div class="input-group mb-3">
+                        <input id="buscar" class="form-control" type="search" placeholder="Buscar" aria-label="Search">
+                        <button id="btn_buscar" class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                </form>
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item mb-2">
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#agregarModal">
+                            Agregar Anime
+                        </button>
+                    </li>
+                    <li class="nav-item">
+                        <button type="button" class="btn btn-danger" id="btn_cerrar_sesion">Cerrar Sesion
+                        </button>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -3,8 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Practica con blade</title>
+    <link rel="stylesheet" href="../css/b5/bootstrap.css">
+    <link rel="stylesheet" href="../css/font_awesome/all.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    <script src="../js/font_awesome/all.js"></script>
+    <script src="../js/b5/popper.js"></script>
+    <script src="../js/b5/bootstrap.js"></script>
+    <script src="../js/jquery/jquery.js"></script>
 </head>
 <body>
     <section class="v-100">
@@ -12,8 +20,6 @@
         <div class="container mt-4">
             @yield('contenido')
         </div>
-    </section>  
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    </section>
 </body>
 </html>
